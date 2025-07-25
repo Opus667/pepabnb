74a211d3939deb817d4439fece5f134a91860b9e
Pépa667 <pepa@pepa.dev.br>
2025-07-25 19:42:29 -0300

    Fim da aula 3

--- back-end/index.js
+++ back-end/index.js
@@ -1,45 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
-import { connectDb } from './config/db.js';
-import User from './models/User.js';
-import bcrypt from 'bcryptjs';
+import UserRoutes from './domains/users/routes.js';
 
 const app = express();
 const { PORT } = process.env;
-const bcryptSalt = bcrypt.genSaltSync();
 
 app.use(express.json());
-
-app.get('/users', async (req, res) => {
-  connectDb();
-
-  try {
-    const userDoc = await User.find();
-
-    res.json(userDoc);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-app.post('/users', async (req, res) => {
-  connectDb();
-
-  const { name, email, password } = req.body;
-  const encryptedPassword = bcrypt.hashSync(password, bcryptSalt);
-
-  try {
-    const newUserDoc = await User.create({
-      name,
-      email,
-      password: encryptedPassword,
-    });
-
-    res.json(newUserDoc);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+app.use('/users', UserRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
