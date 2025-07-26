58b1de3fdaf08fce49399d8890cdb4f267d881a8
Pépa667 <pepa@pepa.dev.br>
2025-07-25 23:56:54 -0300

    Fim da aula 4

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <header className="shadow-md">
       <div className="sm:px8 mx-auto flex max-w-7xl items-center justify-between px-4 py-4">
@@ -39,7 +39,7 @@ const Header = () => {
           </div>
         </Link>
         <Link
-          to="/login"
+          to={user ? "/account" : "/login"}
           className="flex items-center gap-2 rounded-full border border-gray-300 px-4 py-2 pr-4 pl-6 shadow-md"
         >
           <svg
@@ -67,7 +67,11 @@ const Header = () => {
               clipRule="evenodd"
             />
           </svg>
-          <p className="max-w-20 truncate sm:max-w-32">Pepa 667 wwwwwwwwwww</p>
+          {user ? (
+            <p className="max-w-20 truncate sm:max-w-32">{user.name}</p>
+          ) : (
+            <></>
+          )}
         </Link>
       </div>
     </header>
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 
-const Login = ({ user, setUser }) => {
+const Register = ({ setUser }) => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
@@ -10,30 +11,37 @@ const Login = ({ user, setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (email && password) {
-      try {
-        const { data: userDoc } = await axios.post("/users/login", {
-          email,
-          password,
-        });
+    // if (email && password) {
+    //   try {
+    //     const { data: userDoc } = await axios.post("/users/login", {
+    //       email,
+    //       password,
+    //     });
 
-        setUser(userDoc);
-        setRedirect(true);
-      } catch (error) {
-        alert(`erro ao logar; ${error.response.data}`);
-      }
-    } else {
-      alert("preencha tudo!!!");
-    }
+    //     setUser(userDoc);
+    //     setRedirect(true);
+    //   } catch (error) {
+    //     alert(`erro ao logar; ${error.response.data}`);
+    //   }
+    // } else {
+    //   alert("preencha tudo!!!");
+    // }
   };
 
-  if (redirect || user) return <Navigate to="/" />;
+  if (redirect) return <Navigate to="/" />;
 
   return (
     <section className="flex items-center">
       <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4">
-        <h1 className="text-3xl font-bold">Faça seu login</h1>
+        <h1 className="text-3xl font-bold">Faça seu cadastro</h1>
         <form className="flex w-full flex-col gap-2" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="w-full rounded-full border border-gray-300 px-4 py-2"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <input
             type="email"
             className="w-full rounded-full border border-gray-300 px-4 py-2"
@@ -49,14 +57,14 @@ const Login = ({ user, setUser }) => {
             onChange={(e) => setPassword(e.target.value)}
           />
           <button className="bg-primary-400 hover:bg-primary-500 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 font-bold text-white">
-            Login
+            Registrar
           </button>
         </form>
 
         <p>
-          Ainda nao tem conta?{" "}
-          <Link to="/register" className="font-semibold underline">
-            Registre-se aqui!
+          Já tem uma conta?{" "}
+          <Link to="/login" className="font-semibold underline">
+            Logue aqui!
           </Link>
         </p>
       </div>
@@ -64,4 +72,4 @@ const Login = ({ user, setUser }) => {
   );
 };
 
-export default Login;
+export default Register;
