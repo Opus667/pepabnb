d21143224a7bab80249a9931b284fe1695efb607
Pépa667 <pepa@pepa.dev.br>
2025-07-30 23:47:49 -0300

    Intervalo da aula 8

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import UserRoutes from './domains/users/routes.js';
+import PlaceRoutes from './domains/places/routes.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
 app.use('/users', UserRoutes);
+app.use('/places', PlaceRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
